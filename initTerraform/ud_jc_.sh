f307bef5eee855b803f411d01e5015c8cb833e11
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-27 12:04:11 -0400

    Create ud_jc_.sh

---
+++ initTerraform/ud_jc_.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Download the Jenkins, Python & AWS installation scripts
+curl -O https://raw.githubusercontent.com/z0sun/runit/main/jenkins.sh
+curl -O https://raw.githubusercontent.com/z0sun/runit/main/pythoninstall.sh
+curl -O https://raw.githubusercontent.com/z0sun/runit/main/awscli.sh
+
+#Make the scripts executable
+chmod +x jenkins.sh
+chmod +x pythoninstall.sh
+chmod +x awscli.sh
+
+#Run the Jenkins, Python & AWS installation scripts
+./jenkins.sh
+./pythoninstall.sh
+./awscli.sh
+
+sudo apt install build-essential libmysqlclient-dev
