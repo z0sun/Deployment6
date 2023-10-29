d3fdd4a0b694646f83c6edbd9373a32524e27fa9
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-29 19:47:21 -0400

    Update ud_jc.sh

--- defaultTerraform/ud_jc_.sh
+++ defaultTerraform/ud_jc_.sh
@@ -1,17 +1,66 @@
 #!/bin/bash
 
-#Download the Jenkins, Python & AWS installation scripts
-curl -O https://raw.githubusercontent.com/z0sun/runit/main/jenkins.sh
-curl -O https://raw.githubusercontent.com/z0sun/runit/main/awscli.sh
+sudo apt-get update -y
 
-#Make the scripts executable
-chmod +x jenkins.sh
-chmod +x pythoninstall.sh
-chmod +x awscli.sh
+# Install JDK
+sudo apt install openjdk-8-jdk -y
+sudo apt-get update -y
 
-#Run the Jenkins, Python & AWS installation scripts
-./jenkins.sh
-./pythoninstall.sh
-./awscli.sh
+# Download Jenkins keys
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update -y
 
+# Install & start Jenkins
+sudo apt-get install -y fontconfig openjdk-17-jre jenkins
+sudo systemctl start jenkins
+
+# Start Jenkins
+sudo systemctl start jenkins
+
+# Install Python and Database
+sudo apt update
+
+sudo apt install -y software-properties-common
+
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+sudo apt install -y python3.7
+
+sudo apt install -y python3.7-venv
+
+sudo apt install -y build-essential
+
+sudo apt install -y libmysqlclient-dev
+
+sudo apt install -y python3.7-dev
+
+# Install Python3 pip
+sudo apt install python3-pip -y
+
+# Get Jenkins unlock key
+echo "Jenkins Unlock Key:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+# Update packages
+sudo apt update -y
+
+sudo apt update
+sudo apt install unzip
+
+# Download AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install 
+mkdir -p ~/.aws
+
+# Download test file
+curl -O https://dp5-auto1.s3.amazonaws.com/cat.txt
+
+# Download config file from s3 bucket & replace 
+curl https://dp5-auto1.s3.amazonaws.com/config.txt -o config
+cp config ~/.aws/config
+
+sudo apt update
 sudo apt install build-essential libmysqlclient-dev
