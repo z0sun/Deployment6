cd75305aa610122423fa1d5588d7d21ab11ece01
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-29 22:03:45 -0400

    Update ud_jc_.sh

--- defaultTerraform/ud_jc_.sh
+++ defaultTerraform/ud_jc_.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-#Download the Jenkins, Python & AWS installation scripts
-curl -O https://raw.githubusercontent.com/z0sun/runit/main/jenkins.sh
-curl -O https://raw.githubusercontent.com/z0sun/runit/main/awscli.sh
+# Update first before installing 
+# Install Java required for Jenkins
+# -y to fully automize the process 
 
-#Make the scripts executable
-chmod +x jenkins.sh
-chmod +x pythoninstall.sh
-chmod +x awscli.sh
+sudo apt update 
+sudo apt install -y openjdk-11-jre
 
-#Run the Jenkins, Python & AWS installation scripts
-./jenkins.sh
-./pythoninstall.sh
-./awscli.sh
+# Add the required repos before installing jenkins
 
-sudo apt install build-essential libmysqlclient-dev
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+# update system one more time before installing jenkins
+sudo apt update
+sudo apt install -y jenkins
+
+# Start Jenkins
+sudo systemctl start jenkins
+
+sudo apt update
+
+sudo apt install -y software-properties-common
+
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+sudo apt install -y python3.7
+
+sudo apt install -y python3.7-venv
+
+sudo apt install -y build-essential
+
+sudo apt install -y libmysqlclient-dev
+
+sudo apt install -y python3.7-dev
