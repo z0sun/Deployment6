18b6b9d7ba2a3f09c7cd90ace1e4f8a4163ead9a
Joseph White <135557197+z0sun@users.noreply.github.com>
2023-10-29 23:12:16 -0400

    Update main.tf

--- initTerraform/main.tf
+++ initTerraform/main.tf
@@ -1,8 +1,8 @@
 ##### PROVIDER ###########
 provider "aws" {
   region     = "us-east-1"
-  secret_key = var.aws_secret_key
-  access_key = var.aws_access_key
+  secret_key = ""
+  access_key = ""
 }
 
 provider "aws" {
